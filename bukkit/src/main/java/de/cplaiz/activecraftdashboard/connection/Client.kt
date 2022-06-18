ed4d2c41ff2de919297b11f60ce3b87ec972e783
CPlaiz <treesarne@gmail.com>
2022-06-18 03:35:31 +0200

    add: prep accounts

---
+++ bukkit/src/main/java/de/cplaiz/activecraftdashboard/connection/Client.kt
@@ -0,0 +1,7 @@
+package de.cplaiz.activecraftdashboard.connection
+
+class Client {
+
+
+
+}
--- bukkit/src/main/java/de/cplaiz/activecraftdashboard/sql/SQLManager.kt
+++ bukkit/src/main/java/de/cplaiz/activecraftdashboard/sql/SQLManager.kt
@@ -1,4 +1,35 @@
 package de.cplaiz.activecraftdashboard.sql
 
 class SQLManager {
+
+    /*fun init() {
+        // connect to sqlite database
+        Database.connect("jdbc:sqlite:activecraft.db")
+
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(Account)
+
+            Account.insert {
+                it[name] = "admin"
+                it[password] = "admin"
+            }
+        }
+    }
+
+    fun getAccount(name: String, password: String): Account? {
+        return transaction {
+            Account.select { Account.name eq name and Account.password eq password }.firstOrNull()
+        }
+    }
+
+    fun createAccount(name: String, password: String) {
+        transaction {
+            Account.insert {
+                it[name] = "admin"
+                it[password] = "admin"
+            }
+        }
+    }*/
+
 }
