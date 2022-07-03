e69fc583a0edd7825ac091b008a40f0cba458bda
CPlaiz <treesarne@gmail.com>
2022-07-03 23:05:33 +0200

    feat: add disk space getter

--- bukkit/src/main/java/de/cplaiz/activecraftdashboard/monitor/HardwareMonitor.kt
+++ bukkit/src/main/java/de/cplaiz/activecraftdashboard/monitor/HardwareMonitor.kt
@@ -5,10 +5,14 @@ import de.cplaiz.activecraftdashboard.api.Routed
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.bukkit.Bukkit
 import java.lang.management.ManagementFactory
-import kotlin.math.pow
+import java.nio.file.Files
+import javax.swing.filechooser.FileSystemView
 
 object HardwareMonitor : Routed("/monitor") {
+
+    private const val BYTE_TO_MB = 1048576
 
     override fun Route.handleReq() {
         get("/cpu") {
@@ -17,20 +21,35 @@ object HardwareMonitor : Routed("/monitor") {
         get("/memory") {
             call.respondText(getRam().toString())
         }
+        get("/disk") {
+            call.respondText(getDiskSpace().toString())
+        }
+
     }
 
-    fun getProcessCpuLoad() : Double {
+    fun getProcessCpuLoad(): Double {
         return ManagementFactory.getPlatformMXBean(
             OperatingSystemMXBean::class.java
         ).cpuLoad
     }
 
-    fun getRam() : List<Long>{
+    fun getRam(): List<Long> {
         val runtime = Runtime.getRuntime()
-        val divisor = 32.0.pow(4.0)
-        val used: Long = ((runtime.totalMemory() - runtime.freeMemory()).toDouble() / divisor).toLong()
-        val max: Long = (runtime.totalMemory().toDouble() / divisor).toLong()
-        val free: Long = (runtime.freeMemory().toDouble() / divisor).toLong()
+        val used: Long = ((runtime.totalMemory() - runtime.freeMemory()).toDouble() / BYTE_TO_MB).toLong()
+        val max: Long = (runtime.totalMemory().toDouble() / BYTE_TO_MB).toLong()
+        val free: Long = (runtime.freeMemory().toDouble() / BYTE_TO_MB).toLong()
         return listOf(used, free, max)
     }
+
+    fun getDiskSpace(): List<Long> {
+        val fileSystem = FileSystemView.getFileSystemView().defaultDirectory
+        val free: Long = fileSystem.freeSpace
+        val total: Long = fileSystem.totalSpace
+        val minecraft: Long = Files.walk(Bukkit.getWorldContainer().toPath())
+            .map { it.toFile() }
+            .filter { it.isFile }
+            .mapToLong { f -> f.length() }.sum();
+        return listOf(minecraft / BYTE_TO_MB, free / BYTE_TO_MB, total / BYTE_TO_MB)
+    }
+
 }
