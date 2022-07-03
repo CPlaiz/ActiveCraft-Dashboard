c43647ce6095f3d4bf6553df6303baead314293f
CPlaiz <treesarne@gmail.com>
2022-07-03 23:06:57 +0200

    refactor: change null init to lateinit

--- bukkit/src/main/java/de/cplaiz/activecraftdashboard/ActiveCraftDashboard.kt
+++ bukkit/src/main/java/de/cplaiz/activecraftdashboard/ActiveCraftDashboard.kt
@@ -1,15 +1,24 @@
 package de.cplaiz.activecraftdashboard
 
 import de.cplaiz.activecraftcore.ActiveCraftPlugin
+import de.cplaiz.activecraftcore.playermanagement.Profile
 import de.cplaiz.activecraftcore.utils.config.ConfigManager
+import de.cplaiz.activecraftdashboard.account.Account
+import de.cplaiz.activecraftdashboard.account.Accounts
+import de.cplaiz.activecraftdashboard.account.device.DeviceManager
 import de.cplaiz.activecraftdashboard.api.ServerManager
+import de.cplaiz.activecraftdashboard.command.RegistrationCodeCommand
+import de.cplaiz.activecraftdashboard.command.RestartHTTPServerCommand
+import de.cplaiz.activecraftdashboard.sql.SQLManager
 import de.cplaiz.activecraftdashboard.util.config.MainConfig
-import org.bukkit.Bukkit
 
 class ActiveCraftDashboard() : ActiveCraftPlugin() {
 
-    private var servMan: ServerManager? = null
+    private lateinit var servMan: ServerManager //TODO: test if lateinit works; else init null and set in onEnable()
+    private lateinit var sqlMan: SQLManager //TODO: test if lateinit works; else init null and set in onEnable()
+    lateinit var deviceMan: DeviceManager //TODO: test if lateinit works; else init null and set in onEnable()
     lateinit var mainConfig: MainConfig
+    lateinit var accounts: MutableMap<Profile, Account>
 
     companion object {
         lateinit var instance: ActiveCraftDashboard
@@ -31,7 +40,7 @@ class ActiveCraftDashboard() : ActiveCraftPlugin() {
     }
 
     override fun onPluginDisabled() {
-        servMan!!.stopServer()
+        servMan.stopServer()
     }
 
     override fun registerConfigs() {
