4b24bd7064119e171182a0a0ec7427e7b141bc74
CPlaiz <treesarne@gmail.com>
2022-07-09 16:30:47 +0200

    feat: add JSON parser

--- app/activecraft_dashboard/lib/account.dart
+++ app/activecraft_dashboard/lib/account.dart
@@ -1,12 +1,36 @@
+import 'dart:convert';
 import 'package:activecraft_dashboard/permission.dart';
 
 class Account {
 
-  Account(this.name, this.deviceId, this.nickname, this.permissions);
+  Account({required this.name, required this.deviceId, nickname, required this.permissions}) : nickname = nickname ?? name;
 
   final String deviceId;
   final String name;
   final String nickname;
-  final List<Permission> permissions;
+  final Set<Permission> permissions;
+
+  String toJson() {
+    return json.encode({
+      "name": name,
+      "device-id": deviceId,
+      "nickname": nickname,
+      "permissions": permissions.map((p) => p.index).toList()
+    });
+  }
+
+  static fromJson(String json) {
+    Map<String, dynamic> map = jsonDecode(json);
+    Set<Permission> permissions = {};
+    for (final perm in map["permissions"]) {
+      permissions.add(Permission.values[perm]);
+    }
+    return Account(
+      name: map["name"],
+      deviceId: map["device-id"],
+      nickname: map["nickname"],
+      permissions: permissions
+    );
+  }
 
 }
--- app/activecraft_dashboard/lib/server.dart
+++ app/activecraft_dashboard/lib/server.dart
@@ -1,4 +1,5 @@
 import 'account.dart';
+import 'dart:convert';
 
 class Server {
 
@@ -9,7 +10,30 @@ class Server {
   Account account;
   ServerStatus status;
 
-  Server(this.name, this.id, this.ip, this.port, this.account, this.status);
+  Server({this.name = "Minecraft Server", required this.id, required this.ip, required this.port, required this.account, this.status = ServerStatus.OFFLINE});
+
+  String toJson() {
+    return json.encode({
+      "name": name,
+      "id": id,
+      "ip": ip,
+      "port": port,
+      "account": account.toJson(),
+      "status": status.index
+    });
+  }
+
+  static Server fromJson(String json) {
+    Map<String, dynamic> map = jsonDecode(json);
+    return Server(
+      name: map["name"],
+      id: map["id"],
+      ip: map["ip"],
+      port: map["port"],
+      account: Account.fromJson(map["account"]),
+      status: ServerStatus.values[map["status"]]
+    );
+  }
 }
 
 enum ServerStatus {
